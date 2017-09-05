7c1beae850ab3f271ad3b9e0c37947f57476ec28
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2017-09-05 10:25:55 -0300

    Adicionado classe Person

---
+++ ForgingAhead/ForgingAhead/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForgingAhead.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+    }
+}
